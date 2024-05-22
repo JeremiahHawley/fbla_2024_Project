c9b85651bdca3d4f8b701580e7f2c7a81177b586
kevinsullivan0 <kevinsulli@icloud.com>
2024-05-22 13:56:04 -0600

    add surrealdb and dependencies; create speedb_impl.rs with a rough outline for beginning to use surrealdb

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@
 /* this is the partner struct that will be the datatype for each individual "partner", 
 it'll make data management easier and more straightforward (entire database can be one vector of partners)*/ 
 struct partner {
-    //name will be used for the name of the actual partner whether that is the name of the person like "John" or a business like "DJ's"
+    // name will be used for the name of the actual partner whether that is the name of the person like "John" or a business like "DJ's"
     name: String,
     // values will be the list of scholarships/donations/etc that the partner has using a vector of enums that implement the scholarship/donation structs
     values: Vec<partner_value_type>,
@@ -31,12 +31,12 @@ enum partner_value_type {
 
 
 fn read_column(filename: &str, column_name: &str) -> Vec<String>{
-    // return a vector of the string valies of the column where the top value of the column is column_name
+    // return a vector of the string values of the column where the top value of the column is column_name
     return vec!["test".to_string()]
 }
 
 fn read_row(filename: &str, row_name: &str) -> Vec<String>{
-    //return the vector of the string valies of the row where the leftmost value of the column is row_name
+    // return the vector of the string values of the row where the leftmost value of the column is row_name
     return vec!["test".to_string()]
 }
 
@@ -56,3 +56,14 @@ fn main() {
     };
     println!("main ran!   {}",test.name);
 }
+
+// =============
+// PROJECT NOTES/IDEAS
+// =============
+//
+// I'm thinking that we should use a database like surrealdb instead of just csv files because since
+// each donation or scholarship has its own data, that would require either a 3d csv of sorts, putting an 
+// array within on "cell" if it was opened in excel. Instead I'm thinking that we can use surrealdb, query that
+// for displaying the data, and then create a function that can be used to import a csv into the database, though
+// I'm not sure how that would work. I just think that using csv files for the whole thing, perhaps needing
+// to create a relational database ourselves (maybe) would get messy quickly.
