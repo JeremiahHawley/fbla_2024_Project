3c259ae923838d9df10e9824129dd9bf6c35a140
kevinsullivan0 <kevinsulli@icloud.com>
2024-05-24 14:04:16 -0600

    added slint dependencies; modified main.rs to create slint window; began slint window framework; has basic non-interactive window

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,30 @@
 // autosave feature
 // q&a feature
 // encrypt and decrypt 
-// 
-mod gui;
+
+// CURRENTLY EXCLUDED SINCE USING ui/appwindow.slint instead of gui.rs though may use gui.rs for some functions
+// mod gui;
+
 mod csv;
+
+/* 
 
 fn main() {
     csv::hello_csv();
     gui::hello_gui();
+}
+*/
+
+slint::include_modules!();
+
+fn main() -> Result<(), slint::PlatformError> {
+    let ui = AppWindow::new()?;
+
+    let ui_handle = ui.as_weak(); // currently unused but will be used for interactive ui
+    /* ui. on_request_increase_value (move || {
+        let ui = ui_handle.unwrap();
+        ui. set_counter(ui.get_counter () + 1);
+    }); */
+
+    ui.run()
 }
