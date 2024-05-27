eaebd681afcf247d33479f028fd9a79cff905c2c
kevinsullivan0 <kevinsulli@icloud.com>
2024-05-27 17:01:21 -0600

    cleaned up functions

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,7 @@ fn main() {
 */
 
 slint::include_modules!();
-use slint::{Model, ModelRc, StandardListViewItem, TableColumn, VecModel, SharedString, SortModel};
+use slint::{ ModelRc, StandardListViewItem, TableColumn, VecModel, SharedString};
 use std::rc::Rc;
 
 
@@ -18,59 +18,31 @@ use std::rc::Rc;
 fn main() -> Result<(), slint::PlatformError> {
     let ui = AppWindow::new()?;
 
+    // This can be set the the Vec<&str> of headers from the csv file
+    let header_list: Vec<&str> = vec![ 
+        // test data
+        "Header 1",
+        "Header 2",
+        "Header 3"
+    ];
 
-
-    let from_body: Vec<Vec<&str>> = vec![ // TODO: rename
+    // This can be set to the Vec<Vec<&str>> of rows from the csv file
+    let body_list: Vec<Vec<&str>> = vec![ 
+        // test data
         vec!["1", "2", "3"],
         vec!["4", "5", "6"],
         vec!["7", "8", "9"],
     ];
 
-    // THIS WORKS DON'T TOUCH
-    let transition1: Vec<Vec<StandardListViewItem>> = from_body.into_iter().map(|row: Vec<&str>| {
-        row.into_iter().map(|cell: &str| {
-            StandardListViewItem::from(SharedString::from(cell.to_string()))
-        }).collect()
-    }).collect();
 
-    let transition2: Vec<VecModel<StandardListViewItem>> = transition1.into_iter().map(|row: Vec<StandardListViewItem>| {
-        VecModel::from(row)
-    }).collect();
+    // Convert the data from the csv file into types that slint uses
+    let transformed_header_list = transform_header_list(header_list);
+    let transformed_body_list = transform_body_list(body_list);    
 
-    let transition3: Vec<Rc<VecModel<StandardListViewItem>>> = transition2.into_iter().map(|row: VecModel<StandardListViewItem>| {
-        Rc::from(row)
-    }).collect();
-
-    let transition4: Vec<ModelRc<StandardListViewItem>> = transition3.into_iter().map(|row: Rc<VecModel<StandardListViewItem>>| {
-        ModelRc::from(row)
-    }).collect();
-
-    let transition5: VecModel<ModelRc<StandardListViewItem>> = VecModel::from(transition4);
-    let transition6: Rc<VecModel<ModelRc<StandardListViewItem>>> = Rc::from(transition5);
-    let transition7: ModelRc<ModelRc<StandardListViewItem>> = ModelRc::from(transition6);
-
-
-
-    ui.set_table_data(transition7);
-
-
-    let from_header: Vec<&str> = vec![ // TODO: rename
-        "Header 1",
-        "Header 2",
-        "Header 3"
-    ];
-    
-    // THIS WORKS DON'T TOUCH
-    let header_transition1: Vec<TableColumn> = from_header.into_iter().map(|cell: &str| {
-        let mut column: TableColumn = TableColumn::default();
-        column.title = SharedString::from(cell);
-        column
-    }).collect();
-    let header_transition2: VecModel<TableColumn> = VecModel::from(header_transition1);
-    let header_transition3: Rc<VecModel<TableColumn>> = Rc::from(header_transition2);
-    let header_transition4: ModelRc<TableColumn> = ModelRc::from(header_transition3);
-
-    ui.set_header_data(header_transition4);
+    // Set the table data
+    ui.set_header_data(transformed_header_list); // Column headers
+    ui.set_table_data(transformed_body_list); // All other cells
+  
     
 
 
@@ -83,4 +55,38 @@ fn main() -> Result<(), slint::PlatformError> {
     ui.run()
 }
 
+fn transform_header_list(header_list: Vec<&str>) -> ModelRc<TableColumn> {
+    // THIS WORKS DON'T TOUCH
+    let header_transition1: Vec<TableColumn> = header_list.into_iter().map(|cell: &str| {
+        let mut column: TableColumn = TableColumn::default();
+        column.title = SharedString::from(cell);
+        column
+    }).collect();
+    let header_transition2: VecModel<TableColumn> = VecModel::from(header_transition1);
+    let header_transition3: Rc<VecModel<TableColumn>> = Rc::from(header_transition2);
+    let header_transition_final: ModelRc<TableColumn> = ModelRc::from(header_transition3);
+    return header_transition_final;
+}
+
+fn transform_body_list(body_list: Vec<Vec<&str>>) -> ModelRc<ModelRc<StandardListViewItem>> {
+    // THIS WORKS DON'T TOUCH
+    let body_transition1: Vec<Vec<StandardListViewItem>> = body_list.into_iter().map(|row: Vec<&str>| {
+        row.into_iter().map(|cell: &str| {
+            StandardListViewItem::from(SharedString::from(cell.to_string()))
+        }).collect()
+    }).collect();
+    let body_transition2: Vec<VecModel<StandardListViewItem>> = body_transition1.into_iter().map(|row: Vec<StandardListViewItem>| {
+        VecModel::from(row)
+    }).collect();
+    let body_transition3: Vec<Rc<VecModel<StandardListViewItem>>> = body_transition2.into_iter().map(|row: VecModel<StandardListViewItem>| {
+        Rc::from(row)
+    }).collect();
+    let body_transition4: Vec<ModelRc<StandardListViewItem>> = body_transition3.into_iter().map(|row: Rc<VecModel<StandardListViewItem>>| {
+        ModelRc::from(row)
+    }).collect();
+    let body_transition5: VecModel<ModelRc<StandardListViewItem>> = VecModel::from(body_transition4);
+    let body_transition6: Rc<VecModel<ModelRc<StandardListViewItem>>> = Rc::from(body_transition5);
+    let body_transition_final: ModelRc<ModelRc<StandardListViewItem>> = ModelRc::from(body_transition6);
+    return body_transition_final;
+}
 
