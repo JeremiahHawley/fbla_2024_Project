8037b83d2579323939b13c778e723b9b6612a942
kevinsullivan0 <kevinsulli@icloud.com>
2024-05-27 16:38:34 -0600

    table population works now

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,7 @@ fn main() {
 */
 
 slint::include_modules!();
-use slint::{Model, ModelRc, StandardListViewItem, TableColumn, VecModel, SharedString};
+use slint::{Model, ModelRc, StandardListViewItem, TableColumn, VecModel, SharedString, SortModel};
 use std::rc::Rc;
 
 
@@ -20,28 +20,58 @@ fn main() -> Result<(), slint::PlatformError> {
 
 
 
-
-
-    /* PREVIOUS IMPLEMENTATION 
     let from_body: Vec<Vec<&str>> = vec![ // TODO: rename
         vec!["1", "2", "3"],
         vec!["4", "5", "6"],
         vec!["7", "8", "9"],
     ];
-    let body_model : Rc<VecModel<Vec<&str>>> = Rc::new(VecModel::from(from_body.clone()));
-    let body_model_rc: ModelRc<ModelRc<StandardListViewItem>> = ModelRc::new(body_model.clone().into());
-    
+
+    // THIS WORKS DON'T TOUCH
+    let transition1: Vec<Vec<StandardListViewItem>> = from_body.into_iter().map(|row: Vec<&str>| {
+        row.into_iter().map(|cell: &str| {
+            StandardListViewItem::from(SharedString::from(cell.to_string()))
+        }).collect()
+    }).collect();
+
+    let transition2: Vec<VecModel<StandardListViewItem>> = transition1.into_iter().map(|row: Vec<StandardListViewItem>| {
+        VecModel::from(row)
+    }).collect();
+
+    let transition3: Vec<Rc<VecModel<StandardListViewItem>>> = transition2.into_iter().map(|row: VecModel<StandardListViewItem>| {
+        Rc::from(row)
+    }).collect();
+
+    let transition4: Vec<ModelRc<StandardListViewItem>> = transition3.into_iter().map(|row: Rc<VecModel<StandardListViewItem>>| {
+        ModelRc::from(row)
+    }).collect();
+
+    let transition5: VecModel<ModelRc<StandardListViewItem>> = VecModel::from(transition4);
+    let transition6: Rc<VecModel<ModelRc<StandardListViewItem>>> = Rc::from(transition5);
+    let transition7: ModelRc<ModelRc<StandardListViewItem>> = ModelRc::from(transition6);
+
+
+
+    ui.set_table_data(transition7);
+
+
     let from_header: Vec<&str> = vec![ // TODO: rename
         "Header 1",
         "Header 2",
         "Header 3"
     ];
-    let header_model : Rc<VecModel<&str>> = Rc::new(VecModel::from(from_header.clone()));
-    let header_model_rc = ModelRc::from(header_model.into());
+    
+    // THIS WORKS DON'T TOUCH
+    let header_transition1: Vec<TableColumn> = from_header.into_iter().map(|cell: &str| {
+        let mut column: TableColumn = TableColumn::default();
+        column.title = SharedString::from(cell);
+        column
+    }).collect();
+    let header_transition2: VecModel<TableColumn> = VecModel::from(header_transition1);
+    let header_transition3: Rc<VecModel<TableColumn>> = Rc::from(header_transition2);
+    let header_transition4: ModelRc<TableColumn> = ModelRc::from(header_transition3);
 
-    ui.set_table_data(body_model_rc);
-    ui.set_header_data(header_model_rc);
-    */
+    ui.set_header_data(header_transition4);
+    
 
 
     let ui_handle = ui.as_weak(); // currently unused but will be used for interactive ui
